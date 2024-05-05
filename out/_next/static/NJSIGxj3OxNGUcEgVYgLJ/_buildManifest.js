9b13fce586ba7c539272e8419f8b9c46b52e1f44
Lordshady <lordshady1000@.com>
2024-05-05 17:56:04 +0100

    removed out from git ignore

---
+++ out/_next/static/NJSIGxj3OxNGUcEgVYgLJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-a5f2e9cac888aad4.js"],"/_error":["static/chunks/pages/_error-735dc60b184b15e5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
